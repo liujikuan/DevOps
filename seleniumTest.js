c482312b2db9365c364bf45c97231865c59cb215
liujikuan <liujikuan.job@gmail.com>
2024-05-30 14:21:50 +1000

    change to Redis wholly

--- seleniumTest.js
+++ seleniumTest.js
@@ -1,18 +1,40 @@
-var webdriver = require('selenium-webdriver');
+const redis = require("redis");
 
-function googleSearch() {
+// Creates a new Redis client
+// If REDIS_HOST is not set, the default host is localhost
+// If REDIS_PORT is not set, the default port is 6379
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+});
 
-    var driver = new webdriver.Builder().forBrowser('chrome').build();
+redisClient.on("error", (err) => console.log("Error", err));
 
-    driver.get('http://localhost:8080').then(function () {
-        driver.findElement(webdriver.By.linkText('Sign In')).click().then(function () {
-            driver.getTitle().then(function (title) {
-                setTimeout(function () {
-                    console.log(title);
-                    driver.quit();
-                }, 5000);
-            });
-        });
+(async () => {
+  await redisClient.connect();
+
+  // Sets the key "octocat" to a value of "Mona the octocat"
+  const setKeyReply = await redisClient.set("octocat", "Mona the Octocat");
+  console.log("Reply: " + setKeyReply);
+  // Sets a key to "species", field to "octocat", and "value" to "Cat and Octopus"
+  const SetFieldOctocatReply = await redisClient.hSet("species", "octocat", "Cat and Octopus");
+  console.log("Reply: " + SetFieldOctocatReply);
+  // Sets a key to "species", field to "dinotocat", and "value" to "Dinosaur and Octopus"
+  const SetFieldDinotocatReply = await redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus");
+  console.log("Reply: " + SetFieldDinotocatReply);
+  // Sets a key to "species", field to "robotocat", and "value" to "Cat and Robot"
+  const SetFieldRobotocatReply = await redisClient.hSet("species", "robotocat", "Cat and Robot");
+  console.log("Reply: " + SetFieldRobotocatReply);
+
+  try {
+    // Gets all fields in "species" key
+    const replies = await redisClient.hKeys("species");
+    console.log(replies.length + " replies:");
+    replies.forEach((reply, i) => {
+        console.log("    " + i + ": " + reply);
     });
-}
-googleSearch();
+    await redisClient.quit();
+  }
+  catch (err) {
+    // statements to handle any exceptions
+  }
+})();
